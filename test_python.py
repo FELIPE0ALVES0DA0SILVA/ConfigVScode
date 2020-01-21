03b37230ab3e1944618a2a7bdfb35d84b854a0ff
Felipe Alves da Silva <felipe2018cannavarro@gmail.com>
2020-01-20 21:29:40 -0300

    I'm config my testing extension

---
+++ test_python.py
@@ -0,0 +1,17 @@
+def calcular(x, y):
+    """
+    Essa calculadora, calcula alguma coisa.
+    
+    """
+    x = x * 3
+    y = x ** 2 + y
+    return y
+import pytest
+
+class test_testando():
+
+    def  test_calcular():
+        assert calcular(3, 2) == 83
+    
+    def test_calculo():
+        assert calcular(3, 2) == 84
